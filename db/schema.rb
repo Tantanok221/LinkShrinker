5b101c9f2f35f02f5324980100ee1234c1e2c73a
Tantanok221 <6572withspicy@gmail.com>
2025-01-23 10:41:38 +0800

    dev: schema migration

---
+++ db/schema.rb
@@ -0,0 +1,38 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_01_23_022912) do
+  create_table "clicks", force: :cascade do |t|
+    t.integer "short_link_id", null: false
+    t.string "ip_address"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.string "user_agent"
+    t.string "referrer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["short_link_id"], name: "index_clicks_on_short_link_id"
+  end
+
+  create_table "short_links", force: :cascade do |t|
+    t.text "target_url"
+    t.string "short_code"
+    t.string "title"
+    t.integer "clicks_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["short_code"], name: "index_short_links_on_short_code"
+  end
+
+  add_foreign_key "clicks", "short_links"
+end
