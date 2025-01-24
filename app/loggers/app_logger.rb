c9bf35cb38b9ac57639f3cd812d8857b171b9e68
Tantanok221 <6572withspicy@gmail.com>
2025-01-24 22:25:47 +0800

    feat: added simple json logger

---
+++ app/loggers/app_logger.rb
@@ -0,0 +1,26 @@
+class AppLogger
+  def initialize(name)
+    @id = name.to_s
+  end
+
+  def info(message, details = {})
+    log(:info, message, details)
+  end
+
+  def error(message, details = {})
+    log(:error, message, details)
+  end
+
+  private
+
+  def log(level, message, details)
+    Rails.logger.send(level) do
+      {
+        identification: @id,
+        message: message,
+        details: details.transform_keys(&:to_sym),
+        timestamp: Time.current.utc
+      }.to_json
+    end
+  end
+end
