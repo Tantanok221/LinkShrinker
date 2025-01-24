3f08ec15282d4dcae2d29fb6a29f17939e6ddce6
Tantanok221 <6572withspicy@gmail.com>
2025-01-24 17:34:55 +0800

    feat: realtime broadcast clicks

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,35 +1,20 @@
 class AnalyticsController < ApplicationController
   def show
     @short_link = ShortLink.includes(:clicks).find_by!(short_code: params[:short_code])
+    @clicks_by_day = @short_link.clicks.group_by_day(:created_at).count
+    @clicks_by_week = @short_link.clicks.group_by_week(:created_at).count
+    @clicks_by_month = @short_link.clicks.group_by_month(:created_at).count
 
-    analytics_data = {
-      time: {
-        daily: @short_link.clicks.group_by_day(:created_at).count,
-        weekly: @short_link.clicks.group_by_week(:created_at).count,
-        monthly: @short_link.clicks.group_by_month(:created_at).count
-      },
-      geo: {
-        country: @short_link.clicks.group(:country).count,
-        region: @short_link.clicks.group(:region).count,
-        city: @short_link.clicks.group(:city).count
-      },
-      referrers: @short_link.clicks.group(:referrer).count
-    }
+    @clicks_by_country = @short_link.clicks.group(:country).count
+    @clicks_by_region = @short_link.clicks.group(:region).count
+    @clicks_by_city = @short_link.clicks.group(:city).count
 
+    @clicks_by_referrer = @short_link.clicks.group(:referrer).count
     respond_to do |format|
-      format.html do
-        render(AnalyticsComponent.new(
-          short_link: @short_link,
-          total_clicks: @short_link.clicks.count,
-          time_data: analytics_data[:time],
-          geo_data: analytics_data[:geo],
-          referrer_data: analytics_data[:referrers]
-        ))
-      end
+      format.html
       format.turbo_stream
+      end
     end
-
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: "Analytics not found"
   end
-end
--- app/models/click.rb
+++ app/models/click.rb
@@ -1,3 +1,30 @@
 class Click < ApplicationRecord
   belongs_to :short_link
+  after_create_commit :broadcast_analytics_update
+
+  private
+
+  def broadcast_analytics_update
+    Turbo::StreamsChannel.broadcast_update_to(
+      "analytic/#{short_link.short_code}",
+      target: "analytics",
+      html: ApplicationController.render(
+        AnalyticsComponent.new(
+          short_link: short_link,
+          total_clicks: short_link.clicks.count,
+          time_data: {
+            daily: short_link.clicks.group_by_day(:created_at).count,
+            weekly: short_link.clicks.group_by_week(:created_at).count,
+            monthly: short_link.clicks.group_by_month(:created_at).count
+          },
+          geo_data: {
+            country: short_link.clicks.group(:country).count,
+            region: short_link.clicks.group(:region).count,
+            city: short_link.clicks.group(:city).count
+          },
+          referrer_data: short_link.clicks.group(:referrer).count
+        )
+      )
+    )
+  end
 end
