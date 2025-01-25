63a5353d4b6b8ea1a5a8408602f44fd73c02efb4
Tantanok221 <6572withspicy@gmail.com>
2025-01-25 16:43:04 +0800

    dev: added require_master_key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
+  config.require_master_key = true
 
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
