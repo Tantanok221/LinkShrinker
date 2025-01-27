b95eb3585c10387b2a33b12c845cb54ab409b8be
Tantanok221 <6572withspicy@gmail.com>
2025-01-27 23:29:08 +0800

    feat: added ability to add class

--- app/components/typography_component.rb
+++ app/components/typography_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TypographyComponent < ViewComponent::Base
-  def initialize(variant: :h1)
+  def initialize(variant: :h1, classes: "")
     @variant = variant
+    @class = classes
     @classes = case variant
                when :h1 then "text-3xl font-semibold font-sans"
                when :h2 then "text-1xl font-semibold font-sans"
