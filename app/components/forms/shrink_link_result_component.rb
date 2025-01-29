94697e9454104a1fef1ddc146686c75d1ae1932a
Tantanok221 <6572withspicy@gmail.com>
2025-01-30 00:23:26 +0800

    feat: added title presentation on ui

--- app/components/forms/shrink_link_result_component.rb
+++ app/components/forms/shrink_link_result_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Forms::ShrinkLinkResultComponent < ViewComponent::Base
-  def initialize(redirect_link:, short_code:, target_url:)
+  def initialize(redirect_link:, short_code:, target_url:, title:)
     @redirect_link = redirect_link
     @short_code = short_code
     @target_url = target_url
+    @title = title
   end
 end
